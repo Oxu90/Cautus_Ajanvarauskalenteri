7c190e28699a30091302d6b873a20ab3fba76f24
Oxu90 <oskari.putkonen@cautus.fi>
2016-01-11 10:51:26 +0200

    15 minuutin slotit ja ale 30min esto

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
+var secret = 'AIzaSyAYYyFWedE8wTE_D5_A1Pwb_3uFHDB8tmc';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cautus Appointment' });
 });
 
 router.get('/testsite', function(req, res, next) {
-  res.render('testsite', { title: 'Cautus Appointment Test' });
+  res.render('testsite', { title: 'Cautus Appointment Test', gcalapk: secret });
 });
 
 module.exports = router;
